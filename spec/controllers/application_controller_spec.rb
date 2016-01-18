7672e90796f145c16c863a695681362d5d8d9a21
Bastien <bastien.babar@gmail.com>
2016-01-18 04:13:45 +0100

    deprecated tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,17 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  context 'GET about' do
-    it "has a 200 status code" do
-      get :about
-      expect(response.status).to eq(200)
-    end
-  end
-
-  context 'GET contact' do
-    it "has a 200 status code" do
-      get :contact
-      expect(response.status).to eq(200)
-    end
-  end
 end

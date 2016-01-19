951cdc6b3e2f151294e7ae86c03b12c632a5c0ca
Bastien <bastien.babar@gmail.com>
2016-01-19 03:22:43 +0100

    UI change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
 
   # ActionMailer Config
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
+Rails.application.config.assets.precompile += %w( jqBootstrapValidation.js )
+Rails.application.config.assets.precompile += %w( contact_me.js )
+Rails.application.config.assets.precompile += %w( custom.js )

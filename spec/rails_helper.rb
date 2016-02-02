876c701924f032b5c4ba2ffc88f24b03c443dda1
Bastien <bastien.babar@gmail.com>
2016-02-02 01:44:58 +0100

    added lib to test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,19 +15,9 @@ require 'rspec/rails'
 require 'factory_girl_rails'
 require 'simplecov'
 SimpleCov.start
-#
-# Given that it is always loaded, you are encouraged to keep this file as
-# light-weight as possible. Requiring heavyweight dependencies from this file
-# will add to the boot time of your test suite on EVERY test run, even for an
-# individual file that may not need all of that loaded. Instead, consider making
-# a separate helper file that requires the additional dependencies and performs
-# the additional setup, and require it from the spec files that actually need
-# it.
-#
-# The `.rspec` file also contains a few flags that are not defaults but that
-# users commonly want.
-#
-# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Dir[Rails.root.join('lib/services/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
